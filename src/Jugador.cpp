c569b0251446c09d71958dde403ab147ac2f62dc
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-05-28 13:30:33 -0500

    Se eliminaron archivos C++, para iniciar proyecto en Python

--- src/Jugador.cpp
+++
@@ -1,9 +0,0 @@
-#include "Jugador.h"
-
-int Jugador::contador = 0;
-
-Jugador::Jugador(const std::string& nombre, int edad, int rendimiento)
-        : nombre(nombre), edad(edad), rendimiento(rendimiento) {
-    identificador = ++contador;
-}
-
