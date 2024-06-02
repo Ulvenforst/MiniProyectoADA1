2eb2a1f80d7b429a72e105a0fb9248e54ced9982
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-06-02 15:05:00 -0500

    Se creó archivo de algoritmos

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from classes import Asociacion, Jugador, Equipo, Sede
+
+asociacion = Asociacion()
+
+# Crear jugadores
+jugadores = [
+    Jugador("Sofia García", 21, 66), Jugador("Alejandro Torres", 27, 24),
+    Jugador("Valentina Rodriguez", 19, 15), Jugador("Juan López", 22, 78),
+    Jugador("Martina Martinez", 30, 55), Jugador("Sebastián Pérez", 25, 42),
+    Jugador("Camila Fernández", 24, 36), Jugador("Mateo González", 29, 89),
+    Jugador("Isabella Díaz", 21, 92), Jugador("Daniel Ruiz", 17, 57),
+    Jugador("Luciana Sánchez", 18, 89), Jugador("Lucas Vásquez", 26, 82)
+]
+
+# Crear equipos y sedes
+futbolCali = Equipo("Futbol")
+futbolCali.agregar_jugadores([jugadores[9], jugadores[1]])
+volleyballCali = Equipo("Volleyball")
+volleyballCali.agregar_jugadores([jugadores[0], jugadores[8], jugadores[11], jugadores[5]])
+
+futbolMedellin = Equipo("Futbol")
+futbolMedellin.agregar_jugadores([jugadores[10], jugadores[7], jugadores[6]])
+volleyballMedellin = Equipo("Volleyball")
+volleyballMedellin.agregar_jugadores([jugadores[2], jugadores[3], jugadores[4]])
+
+sedeCali = Sede("Cali")
+sedeCali.equipos.extend([futbolCali, volleyballCali])
+sedeMedellin = Sede("Medellín")
+sedeMedellin.equipos.extend([futbolMedellin, volleyballMedellin])
+
+asociacion.sedes.extend([sedeCali, sedeMedellin])
